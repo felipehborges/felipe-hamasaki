a3aebe9b6cb5101c9a4f945e076127ba1341c153
Hamasaki <felipehama@gmail.com>
2025-03-20 17:40:54 -0300

    Refactor Button component code

--- src/components/ui/custom/button.tsx
+++ src/components/ui/custom/button.tsx
@@ -5,21 +5,18 @@ import { cva, type VariantProps } from 'class-variance-authority'
 import { cn } from '@/lib/utils'
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-[color,box-shadow] disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive hover:cursor-pointer transition-all duration-100 ease-in-out border-2 border-black",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-[color,box-shadow] disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive hover:cursor-pointer transition-all duration-100 ease-in-out border-2 border-black shadow-black shadow-[0px_3px_0px_#000] hover:shadow-none hover:translate-y-1",
   {
     variants: {
       variant: {
-        default:
-          'bg-primary dark:text-primary-foreground hover:bg-primary/90 shadow-black shadow-[3px_3px_0px_#000] hover:shadow-none hover:translate-y-1 hover:translate-x-1',
+        default: 'bg-primary dark:text-primary-foreground',
+        secondary: 'bg-secondary text-secondary-foreground',
+        outline: 'bg-background/80 active:bg-background/50',
         destructive:
-          'bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 shadow-[3px_3px_0px_#000] hover:shadow-none hover:translate-y-1 hover:translate-x-1',
-        outline:
-          'bg-background hover:bg-accent hover:text-accent-foreground shadow-[3px_3px_0px_#000] hover:shadow-none hover:translate-y-1 hover:translate-x-1',
-        secondary:
-          'bg-secondary text-secondary-foreground hover:bg-secondary/80 shadow-[3px_3px_0px_#000] hover:shadow-none hover:translate-y-1 hover:translate-x-1',
-        ghost:
-          'hover:bg-accent hover:text-accent-foreground hover:bg-accent/90 shadow-none border-none',
-        link: 'text-primary underline-offset-4 hover:underline shadow-none border-none'
+          'bg-destructive text-white focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40',
+
+        ghost: 'hover:bg-primary/15 dark:hover:bg-primary/5',
+        link: 'text-primary underline-offset-4 hover:underline'
       },
       size: {
         default: 'h-9 px-4 py-2 has-[>svg]:px-3',
@@ -31,7 +28,13 @@ const buttonVariants = cva(
     defaultVariants: {
       variant: 'default',
       size: 'default'
-    }
+    },
+    compoundVariants: [
+      {
+        variant: ['ghost', 'link'],
+        className: 'shadow-none border-none hover:translate-none'
+      }
+    ]
   }
 )
 
